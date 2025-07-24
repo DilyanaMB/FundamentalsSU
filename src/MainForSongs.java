f12d010a446359a9040dae24b5d0f161a43a08cc
dilyanabodurova <diliana.bodurova@abv.bg>
2025-07-24 17:56:46 +0300

    class and objects

---
+++ src/MainForSongs.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainForSongs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numSongs = Integer.parseInt(sc.nextLine());
+
+        List<String> songs = new ArrayList<>();
+
+        for (int i = 0; i < numSongs; i++) {
+            
+        }
+    }
+}
